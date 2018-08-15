fc3891f5b90b812781ad76c31a9f8f88c55434cb
갓거 <taylored@naver.com>
2018-08-15 16:46:08 +0900

    Transaction Layout Done

---
+++ DrCRYPTO/Controller/Transaction/TransactionCell.swift
@@ -0,0 +1,24 @@
+//
+//  TransactionCell.swift
+//  DrCRYPTO
+//
+//  Created by 갓거 on 2018. 8. 15..
+//  Copyright © 2018년 Dr.CRYPTO. All rights reserved.
+//
+
+import UIKit
+
+class TransactionCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
